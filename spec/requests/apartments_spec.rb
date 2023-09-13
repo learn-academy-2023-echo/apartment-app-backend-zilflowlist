13a931e52a17cc501ccbdca53fa317b0aaf33344
Graham <blundell.gp@gmail.com>
2023-09-13 14:03:53 -0700

    added read func

--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -1,7 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe "Apartments", type: :request do
+  let(:user) { User.create(
+    email: 'test1@example.com',
+    password: 'password',
+    password_confirmation: 'password'
+    )
+  }
+
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'gets a list of apartments' do
+      apartment = user.apartments.create(
+        street: 'Test Lane',
+        unit: '7',
+        city: 'San Deegleby',
+        state: 'CA',
+        square_footage: 800,
+        price: '3,500',
+        bedrooms: 2,
+        bathrooms: 1.5,
+        pets: 'yes',
+        image: 'https://plus.unsplash.com/premium_photo-1672252617539-878656f17efe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'
+      )
+      get '/apartments'
+
+      apartment = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(apartment.first['street']).to eq('Test Lane')
+    end
   end
+
 end
